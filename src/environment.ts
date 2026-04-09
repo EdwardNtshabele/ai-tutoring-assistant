d8e53f72d1081baf7d11937960751557e64d93c2
EdwardNtshabele <145551031+EdwardNtshabele@users.noreply.github.com>
2026-04-09 20:35:10 +0200

    Update environment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost/ai-tutor-backend/api'  // ONLY this - no API keys here!
+  apiUrl: 'http://localhost/ai-tutor-backend/api'
 };
